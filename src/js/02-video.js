1efa5e7692849ca6240d91c98291a93e0943c703
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2022-02-09 12:46:44 +0200

    exper

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+const videoRef = document.querySelector('#vimeo-player');
 
+const iframePlayer = new Player(videoRef);
+// iframePlayer
+//   .play()
+//   .then(function () {
+//     // the video was played
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+};
+let currentTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+let setTime = currentTime ? currentTime : 0;
+iframePlayer.setCurrentTime(setTime.seconds);
+
+iframePlayer.on('timeupdate', throttle(onPlay, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+const throttle = require('lodash.throttle');
 
+const formRef = document.querySelector('.feedback-form');
+// const submitBtn
+
+formRef.addEventListener('input', throttle(formListener, 1000));
+formRef.addEventListener('submit', submitBtn);
+const STORAGE_KEY = 'feedback-form-state';
+
+function formListener(e) {
+  const form = e.target.closest('form');
+  const formData = new FormData(form);
+  const finalData = {};
+
+  // const { email, message } = form.elements;
+  // finalData[email.name] = email.value;
+  // finalData[message.name] = message.value;
+
+  // console.log(form);
+
+  for (const [key, value] of formData.entries()) {
+    finalData[key] = value;
+  }
+  // console.log(finalData);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(finalData));
+}
+
+saveTextArea();
+
+function saveTextArea() {
+  const localData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  const { email, message } = formRef.elements;
+  // console.log(message.value);
+  if (localData) {
+    (email.value = localData.email), (message.value = localData.message);
+  }
+}
+
+function submitBtn(e) {
+  e.preventDefault();
+  const { email, message } = formRef.elements;
+  if (!email.value || !message.value) {
+    alert('Please fill in all the fields!');
+    return;
+  }
+  console.log(`${email.name}: ${email.value}`);
+  console.log(`${message.name}: ${message.value}`);
+
+  localStorage.removeItem(STORAGE_KEY);
+  e.currentTarget.reset();
+}
